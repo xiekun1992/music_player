5917045fc3dc930df805407cddf20191cf0c8d90
xiekun1992 <xkfront@outlook.com>
2019-06-18 00:25:36 +0800

    webgl+ffmpeg实现播放器

---
+++ main.js
@@ -0,0 +1,42 @@
+const ffmpeg = require('../node-addons/node-addon-ffmpeg/build/Release/ffmpeg.node')
+console.log(ffmpeg)
+let info = ffmpeg.config("D:\\Wildlife.wmv")
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize(){
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+        
+var width = info.width, height = info.height, interval = 1000 / info.fps;
+var data = ffmpeg.extractRGBFrame()
+data = new Uint8Array(data)
+
+var texture = new THREE.DataTexture( data, width, height, THREE.RGBFormat );
+var geometry = new THREE.PlaneGeometry( 48, 27, 1, 1 )
+// var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+var material = new THREE.MeshBasicMaterial( { map: texture, side: THREE.DoubleSide, color: 0xffffff } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+cube.rotation.z = Math.PI / 1;
+cube.rotation.y = -Math.PI / 1;
+camera.position.z = 32.7;
+
+setInterval(function() {
+    renderer.render( scene, camera );
+    // var s = performance.now()
+    texture.image.data = new Uint8Array(ffmpeg.extractRGBFrame());
+    texture.needsUpdate = true
+    // console.log(performance.now() - s)
+}, interval);
