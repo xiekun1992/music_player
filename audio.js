d4d811c363e7b7d499c744bd9bd587c3818e0760
xiekun1992 <xkfront@outlook.com>
2019-07-31 00:42:14 +0800

    视音频播放调用修改

--- audio.js
+++ audio.js
@@ -1,69 +1,73 @@
-let worker = new Worker('./audio-worker.js')
+function playAudio() {
+  if (info.audio) {
+    let worker = new Worker('./audio-worker.js')
 
-let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-let channels = +info.audio.channels
-let sampleRate = +info.audio.sampleRate
-let length = 5 * sampleRate // 左右声道各5秒音频缓冲
-let myAudioBuffer = audioCtx.createBuffer(channels, length, sampleRate);
-let nowBuffering1 = myAudioBuffer.getChannelData(0, 16, sampleRate);
-let nowBuffering2 = myAudioBuffer.getChannelData(1, 16, sampleRate);
-let timer
-let source = audioCtx.createBufferSource();
-source.onended = function() {
-  console.log('Your audio has finished playing');
-}
-source.buffer = myAudioBuffer;
-source.loop = true
-source.connect(audioCtx.destination);
-source.start(0);
+    let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    let channels = +info.audio.channels
+    let sampleRate = +info.audio.sampleRate
+    let length = 5 * sampleRate // 左右声道各5秒音频缓冲
+    let myAudioBuffer = audioCtx.createBuffer(channels, length, sampleRate);
+    let nowBuffering1 = myAudioBuffer.getChannelData(0, 16, sampleRate);
+    let nowBuffering2 = myAudioBuffer.getChannelData(1, 16, sampleRate);
+    let timer
+    let source = audioCtx.createBufferSource();
+    source.onended = function() {
+      console.log('Your audio has finished playing');
+    }
+    source.buffer = myAudioBuffer;
+    source.loop = true
+    source.connect(audioCtx.destination);
+    source.start(0);
 
-worker.onmessage = function(event) {
-  console.log(event.data)
-  if (event.data.code == 3) {
-    if (event.data.audioBufferLeft.length <= 0 && event.data.audioBufferRight.length <= 0) {
-      clearInterval(timer)
-      source.stop()
-      source.disconnect()
+    worker.onmessage = function(event) {
+      console.log(event.data)
+      if (event.data.code == 3) {
+        if (event.data.audioBufferLeft.length <= 0 && event.data.audioBufferRight.length <= 0) {
+          clearInterval(timer)
+          source.stop()
+          source.disconnect()
+        }
+        for (let i = 0; i < event.data.audioBufferLeft.length; i++) {
+          let d = event.data.audioBufferLeft[i]
+          nowBuffering1[(event.data.bufferIndex + i) % length] = ((d + 32768) % 65536 - 32768) / 32768.0
+        }
+      
+        for (let i = 0; i < event.data.audioBufferRight.length; i++) {
+          let d = event.data.audioBufferRight[i]
+          nowBuffering2[(event.data.bufferIndex + i) % length] = ((d + 32768) % 65536 - 32768) / 32768.0
+        }
+      } else if (event.data.code == 4) {
+        clearInterval(timer)
+        source.stop()
+        source.disconnect()
+      }
     }
-    for (let i = 0; i < event.data.audioBufferLeft.length; i++) {
-      let d = event.data.audioBufferLeft[i]
-      nowBuffering1[(event.data.bufferIndex + i) % length] = ((d + 32768) % 65536 - 32768) / 32768.0
-    }
-  
-    for (let i = 0; i < event.data.audioBufferRight.length; i++) {
-      let d = event.data.audioBufferRight[i]
-      nowBuffering2[(event.data.bufferIndex + i) % length] = ((d + 32768) % 65536 - 32768) / 32768.0
-    }
-  } else if (event.data.code == 4) {
-    clearInterval(timer)
-    source.stop()
-    source.disconnect()
-  }
-}
-worker.postMessage({
-  code: 2,
-  bufferLength: length,
-  audioBufferLeft: nowBuffering1,
-  audioBufferRight: nowBuffering2
-})
+    worker.postMessage({
+      code: 2,
+      bufferLength: length,
+      audioBufferLeft: nowBuffering1,
+      audioBufferRight: nowBuffering2
+    })
 
-worker.postMessage({
-  code: 1,
-  dataNeed: length
-})
-let prevSecond = 0;
-timer = setInterval(() => {
-  let contextTime = audioCtx.getOutputTimestamp().contextTime
-  let second = Math.floor(contextTime)
-  // ...... 设置音频时钟
-  // console.log(audioCtx.getOutputTimestamp())
-  // ffmpeg.updateAudioClock(contextTime)
-  // 根据时间差替换音频缓冲区内的数据
-  if (second - prevSecond > 2) {
     worker.postMessage({
       code: 1,
-      dataNeed: Math.abs(second - prevSecond) * sampleRate
+      dataNeed: length
     })
-    prevSecond = second
+    let prevSecond = 0;
+    timer = setInterval(() => {
+      let contextTime = audioCtx.getOutputTimestamp().contextTime
+      let second = Math.floor(contextTime)
+      // ...... 设置音频时钟
+      // console.log(audioCtx.getOutputTimestamp())
+      // ffmpeg.updateAudioClock(contextTime)
+      // 根据时间差替换音频缓冲区内的数据
+      if (second - prevSecond > 2) {
+        worker.postMessage({
+          code: 1,
+          dataNeed: Math.abs(second - prevSecond) * sampleRate
+        })
+        prevSecond = second
+      }
+    }, 20)
   }
-}, 20)
+}
--- video.js
+++ video.js
@@ -1,56 +1,61 @@
-// console.log(ffmpeg)
-// let info = ffmpeg.config(filename)
-
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-window.addEventListener( 'resize', onWindowResize, false );
-
-function onWindowResize(){
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-}
+function playVideo() {
+    
+    if (info.video) {
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 0.1, 1000 );
         
-var width = +info.video.width, height = +info.video.height, interval = Math.ceil(1000 / +info.video.fps);
-var data = ffmpeg.decodeVideo()
-data = new Uint8Array(data)
-
-var texture = new THREE.DataTexture( data, width, height, THREE.RGBFormat );
-var geometry = new THREE.PlaneGeometry( 48, 27, 1, 1 )
-// var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-var material = new THREE.MeshBasicMaterial( { map: texture, side: THREE.DoubleSide, color: 0xffffff } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-cube.rotation.z = Math.PI / 1;
-cube.rotation.y = -Math.PI / 1;
-camera.position.z = 32.7;
-
-renderer.render( scene, camera );
-
-// function render() {
-//     renderer.render( scene, camera );
-//     texture.image.data = new Uint8Array(ffmpeg.decodeVideo());
-//     texture.needsUpdate = true
-//     requestAnimationFrame(render)
-// }
-// render()
-
-let vtimer = setInterval(function() {
-    // var s = performance.now()
-    renderer.render( scene, camera );
-    let tmp = ffmpeg.decodeVideo()
-    if (tmp) {
-        texture.image.data = new Uint8Array(tmp);
-        texture.needsUpdate = true
-    } else {
-        clearInterval(vtimer)
+        var renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( renderer.domElement );
+        window.addEventListener( 'resize', onWindowResize, false );
+        
+        function onWindowResize(){
+        
+            camera.aspect = window.innerWidth / window.innerHeight;
+            camera.updateProjectionMatrix();
+        
+            renderer.setSize( window.innerWidth, window.innerHeight );
+        
+        }
+                
+        var width = +info.video.width, height = +info.video.height, interval = Math.ceil(1000 / +info.video.fps);
+        var data = ffmpeg.decodeVideo()
+        data = new Uint8Array(data)
+        
+        var texture = new THREE.DataTexture( data, width, height, THREE.RGBFormat );
+        var geometry = new THREE.PlaneGeometry( 48, 27, 1, 1 )
+        // var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+        var material = new THREE.MeshBasicMaterial( { map: texture, side: THREE.DoubleSide, color: 0xffffff } );
+        var cube = new THREE.Mesh( geometry, material );
+        scene.add( cube );
+        cube.rotation.z = Math.PI / 1;
+        cube.rotation.y = -Math.PI / 1;
+        camera.position.z = 32.7;
+        
+        renderer.render( scene, camera );
+        
+        // function render() {
+        //     renderer.render( scene, camera );
+        //     texture.image.data = new Uint8Array(ffmpeg.decodeVideo());
+        //     texture.needsUpdate = true
+        //     requestAnimationFrame(render)
+        // }
+        // render()
+        
+        let vtimer = setInterval(function() {
+            ffmpeg.decodeVideo()
+            // var s = performance.now()
+            // renderer.render( scene, camera );
+            // data = ffmpeg.decodeVideo()
+            // data = null
+            // if (data) {
+            //     texture.image.data = new Uint8Array(data);
+            //     texture.needsUpdate = true
+            // } else {
+            //     clearInterval(vtimer)
+            // }
+            // console.log(performance.now() - s)
+        }, interval);
     }
-    // console.log(performance.now() - s)
-}, interval);
+
+}    
